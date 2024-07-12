9eebd2c26eb10add49d8c7239e8d10df5d5813f3
Thaer  Saidi <131191852+thaersaidi@users.noreply.github.com>
2024-07-12 10:24:29 +0200

    Update next.config.js

--- src/azurechat/next.config.js
+++ src/azurechat/next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
+  typescript: {
+    // Ignore TypeScript errors during build
+    ignoreBuildErrors: true,
+  },
+  crossOrigin: 'anonymous',
 };
 
 module.exports = nextConfig;
